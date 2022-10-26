5184f2a8af76260113cc81cb25985fc8cffe34f5
cepedagm14 <cepedagm14@gmail.com>
2022-10-25 23:59:34 -0300

    delete mutation

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -16,16 +16,3 @@ export const useClientGQL = (key, query, variables, config = {}) => {
   // const fetchData = async () => await request(endpoint, query, variables);
   return useQuery(key, fetchData, config);
 };
-
-export const useMutationGQL = (key, query, variables, config = {}) => {
-    let endpoint = url;
-    const headers = {
-      headers: {
-        authorization: `${token} `,
-      },
-    };
-    const graphQLClient = new GraphQLClient(endpoint, headers);
-    const fetchData = async () => await graphQLClient.request(query, variables);
-    // const fetchData = async () => await request(endpoint, query, variables);
-    return useMutation(key, fetchData, config);
-  };
